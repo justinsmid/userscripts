595e942168473a4930db1bcc0c6012661b5df0d0
Justin Smid <justin.smid@getmarvia.com>
2023-07-03 13:06:05 +0200

    Add prevUserButton to JIRA improvements

--- jira-improvements.js
+++ jira-improvements.js
@@ -48,7 +48,8 @@
             scrollable.get()[0].addEventListener("scroll", addTitleToEpicBadges)
         }
 
-        // Add next user button
+        // Add prev/next user buttons
+        addPrevUserButton()
         addNextUserButton()
     }
 
@@ -107,9 +108,7 @@
             iconContainer.style.setProperty("width", "0")
         }
 
-        clone.addEventListener("click", () => {
-            const userIds = []
-
+        const selectNextUser = () => {
             const searchParams = new URLSearchParams(window.location.search)
 
             const usersContainer = $("fieldset[data-test-id='filters.ui.filters.assignee.stateless.assignee-filter']")
@@ -218,7 +217,174 @@
 
                 return
             }
+        }
+
+        clone.addEventListener("click", selectNextUser)
+
+        // Add cloned action to DOM
+        filterActionsContainer.append(clone)
+    }
+
+    function addPrevUserButton() {
+        // Bail if our button is already present
+        const PREV_USER_BUTTON_ID = "custon-prev-user-button"
+        if ($(`#${PREV_USER_BUTTON_ID}`).length > 0) {
+            console.warn("'Prev user' button already found. Preventing duplicate.")
+            return
+        }
+
+        // Find existing actions
+        const filterActionsContainer = $("div[data-test-id='software-filters.ui.list-filter-container']").get()[0].firstChild
+        const existingActions = Array.from(filterActionsContainer.children)
+
+        // Find first visible action
+        const actionToClone = existingActions.find((el) => {
+            const style = window.getComputedStyle(el)
+
+            const visible = style.visibility === "visible"
+            const width = style.width
+
+            if (typeof width === "string" && width.endsWith("px")) {
+                const pxWidth = parseInt(width.split("px")[0])
+                return visible && pxWidth > 0
+            }
+
+            return false
         })
+        if (!actionToClone) {
+            console.warn("Could not find a suitable action to clone.")
+            return
+        }
+
+        // Clone found action
+        const clone = actionToClone.cloneNode(true)
+        clone.style.setProperty("order", "0")
+        clone.id = PREV_USER_BUTTON_ID
+
+        // Update clone's text
+        const textContainer = $(clone).find("div[data-testid*='filters.common.ui.list']")
+        textContainer.get()[0].firstChild.textContent = "< Prev user"
+
+        // Remove clone's icon
+        const iconContainer = $(clone).find("span[role='img']").parent().get()[0]
+        if (iconContainer) {
+            iconContainer.style.setProperty("visibility", "hidden")
+            iconContainer.style.setProperty("width", "0")
+        }
+
+        const selectPrevUser = () => {
+            const searchParams = new URLSearchParams(window.location.search)
+
+            const usersContainer = $("fieldset[data-test-id='filters.ui.filters.assignee.stateless.assignee-filter']")
+            const userContainers = Array.from(usersContainer.find("div").first().children())
+
+            const firstUserAvatar = usersContainer.find("input[name='assignee']").first()
+            const lastUserAvatar = usersContainer.find("input[name='assignee']").eq(userContainers.length - 3) // The last few avatars aren't actually user-avatars, so skip those.
+
+            const openUserDropdownAvatarContainer = $(userContainers[userContainers.length - 2])
+            const openUserDropdownAvatar = openUserDropdownAvatarContainer.find("input[name='assignee']")
+
+            if (!searchParams || !searchParams.has("assignee")) {
+                // No current user selected; select first user.
+                firstUserAvatar.click()
+
+                return
+            } else {
+                // User is selected. Determine who it is and select the next user.
+                const encodedUserId = searchParams.get("assignee")
+                const userId = decodeURIComponent(encodedUserId)
+
+                const selectedUserAvatar = usersContainer.find(`input[id='assignee-${userId}']`)
+                if (selectedUserAvatar.length <= 0) {
+                    // If we do have a selected user, but can't find it as part of the avatars, it probably means it's a user in the +n dropdown. Look there.
+                    openUserDropdownAvatar.click()
+
+                    const dropdownContainer = $("[id*='dropdown']")
+                    if (dropdownContainer.length <= 0) {
+                        console.warn("Could not find user dropdown")
+                        $("body").click()
+                        return
+                    }
+
+                    const dropdownItemsContainer = dropdownContainer.find("[role='menu']").children(":first")
+                    const dropdownItems = dropdownItemsContainer.children()
+
+                    // Find selected dropdown item
+                    const selectedDropdownItem = dropdownItems.find(`button[role='menuitemcheckbox'][id='${userId}']`)
+                    if (selectedDropdownItem.length <= 0) {
+                        console.warn("Could not find selected user's button in dropdown")
+                        $("body").click()
+                        return
+                    }
+
+                    // Deselected selected item
+                    selectedDropdownItem.click()
+
+                    // Find prev dropdown item
+                    const selectedDropdownContainer = selectedDropdownItem.parent()
+                    const prevDropdownContainer = selectedDropdownContainer.prev()
+                    if (prevDropdownContainer.length <= 0) {
+                        // If we did find a selected item in the dropdown, but no previous item, it indicates the first user is selected. Select the last user outside of the dropdown
+                        lastUserAvatar.click()
+                        return
+                    }
+
+                    // If we did find a prev dropdown item, select it.
+                    prevDropdownContainer.get()[0].style.setProperty("outline", "2px solid var(--ds-border-focused, #4c9aff)")
+                    const prevDropdownItem = prevDropdownContainer.find("button[role='menuitemcheckbox']")
+                    prevDropdownItem.click()
+
+                    // Close dropdown. Delay by a bit so people can see who got selected in the dropdown.
+                    setTimeout(() => {
+                        $("body").click()
+                    }, 500)
+                } else {
+                    // Otherwise, if we did find a selected user avatar...
+                    const selectedUserContainer = selectedUserAvatar.parent()
+
+                    // Deselect selected user
+                    selectedUserAvatar.click()
+
+                    // Find prev user
+                    const prevUserContainer = selectedUserContainer.prev()
+                    const prevUserAvatar = prevUserContainer.find("input[name='assignee']")
+
+                    const selectedUserIndex = userContainers.indexOf(selectedUserContainer.get()[0])
+                    const isFirstUserAvatar = selectedUserIndex === 0
+                    if (isFirstUserAvatar) {
+                        // When the first user avatar is selected, select the last user from the dropdown instead.
+
+                        // Open the dropdown
+                        openUserDropdownAvatar.click()
+
+                        // Find the last dropdown item
+                        const dropdownContainer = $("[id*='dropdown']")
+                        if (dropdownContainer.length <= 0) return
+
+                        const dropdownItemsContainer = dropdownContainer.find("[role='menu']").children(":first")
+                        const dropdownItems = dropdownItemsContainer.children()
+                        const lastDropdownItem = dropdownItems.last()
+
+                        // Select it
+                        lastDropdownItem.find("button").first().click()
+
+                        // Close the dropdown
+                        setTimeout(() => {
+                            $("body").click()
+                        }, 500)
+
+                        return
+                    } else {
+                        // Otherwise, the previous user's avatar is shown. Select it.
+                        prevUserAvatar.click()
+                    }
+                }
+
+                return
+            }
+        }
+
+        clone.addEventListener("click", selectPrevUser)
 
         // Add cloned action to DOM
         filterActionsContainer.append(clone)
